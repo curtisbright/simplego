0a280655bd188e2b18f2c4a2ed4927f929576471
Curtis Bright <cbright@uwaterloo.ca>
2013-01-27 05:38:32 -0500

    capturing rule

--- go-points.cpp
+++ go-points.cpp
@@ -4,8 +4,8 @@
 int turn;
 int board[19][19];
 
-wxButton* AIButton;
-wxTextCtrl* Depth1Text;
+//wxButton* AIButton;
+//wxTextCtrl* Depth1Text;
 
 /*wxString pointname()
 {	wxString output = "";
@@ -13,6 +13,67 @@ wxTextCtrl* Depth1Text;
 	output += wxString::Format("%d", 19-y);
 	return output;
 }*/
+
+void boardcpy(int cpy[19][19], int src[19][19])
+{	for(int i=0; i<19; i++)
+		for(int j=0; j<19; j++)
+			cpy[i][j] = src[i][j];
+}
+
+void printboard(int board[19][19])
+{	for(int j=0; j<19; j++)
+	{	for(int i=0; i<19; i++)
+		{	if(board[i][j]==0)
+				printf(".");
+			else if(board[i][j]==1)
+				printf("X");
+			else if(board[i][j]==2)
+				printf("O");
+			else if(board[i][j]==3)
+				printf("*");
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void removegroup(int board[19][19], int x, int y)
+{	int colour = board[x][y];
+	board[x][y] = 0;
+	if(x>1 && board[x-1][y]==colour)
+		removegroup(board, x-1, y);
+	if(x<18 && board[x+1][y]==colour)
+		removegroup(board, x+1, y);
+	if(y>1 && board[x][y-1]==colour)
+		removegroup(board, x, y-1);
+	if(y<18 && board[x][y+1]==colour)
+		removegroup(board, x, y+1);
+}
+
+bool hasliberties(int board[19][19], int x, int y)
+{	if(x>1 && board[x-1][y]==0)
+		return true;
+	else if(y>1 && board[x][y-1]==0)
+		return true;
+	else if(x<18 && board[x+1][y]==0)
+		return true;
+	else if(y<18 && board[x][y+1]==0)
+		return true;
+	int colour = board[x][y];
+	int temp[19][19], copy[19][19];
+	boardcpy(temp, board);
+	temp[x][y] = 3;
+	boardcpy(copy, temp);
+	bool result = false;
+	result |= ((x>1 && colour==copy[x-1][y]) ? hasliberties(temp, x-1, y) : false);
+	boardcpy(copy, temp);
+	result |= ((y>1 && colour==copy[x][y-1]) ? hasliberties(temp, x, y-1) : false);
+	boardcpy(copy, temp);
+	result |= ((x<18 && colour==copy[x+1][y]) ? hasliberties(temp, x+1, y) : false);
+	boardcpy(copy, temp);
+	result |= ((y<18 && colour==copy[x][y+1]) ? hasliberties(temp, x, y+1) : false);
+	return result;
+}
 
 class MainPanel : public wxPanel
 {
@@ -39,6 +100,8 @@ void MainPanel::DrawStone(wxDC& dc, int x, int y, int colour)
 		dc.SetBrush(*wxBLACK_BRUSH);
 	else if(colour==2)
 		dc.SetBrush(*wxWHITE_BRUSH);
+	else
+		assert(false);
 	dc.DrawCircle(wxPoint(16*(x+1),16*(y+1)), 6);
 }
 
@@ -102,7 +165,7 @@ MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& si
 	//AIButton = new wxButton(this, wxID_HIGHEST+1, _T("OK"), wxPoint(150, 350));
 	//Connect(wxID_HIGHEST+1, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MainFrame::RunAI));
 	
-	Depth1Text = new wxTextCtrl(this, wxID_HIGHEST+2, _T(""), wxPoint(25, 350), wxDefaultSize, wxTE_PROCESS_ENTER);
+	//Depth1Text = new wxTextCtrl(this, wxID_HIGHEST+2, _T(""), wxPoint(25, 350), wxDefaultSize, wxTE_PROCESS_ENTER);
 	//Connect(wxID_HIGHEST+2, wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler(MainFrame::OnEnter));
 }
 
@@ -141,22 +204,46 @@ void MainPanel::LMouseUp(wxMouseEvent& event)
 {	int x, y;
 	x = (event.GetPosition().x-8)/16;
 	y = (event.GetPosition().y-8)/16;
+	if(x<0 || x>18 || y<0 || y>18)
+		return;
 	wxClientDC dc(this);
+	int temp[19][19];
+	boardcpy(temp, board);
 	if(board[x][y]==0)
-	{	turn = (turn+1)%2;
-		board[x][y] = turn+1;
-		DrawStone(dc, x, y, turn+1);
+	{	temp[x][y] = (turn+1)%2+1;
+		if(hasliberties(temp, x, y) || (x>1 && !hasliberties(temp, x-1, y))
+		                            || (x<18 && !hasliberties(temp, x+1, y))
+		                            || (y>1 && !hasliberties(temp, x, y-1))
+		                            || (y<18 && !hasliberties(temp, x, y+1)))
+		{	turn = (turn+1)%2;
+			board[x][y] = turn+1;
+			DrawStone(dc, x, y, turn+1);
+			if(x>1 && !hasliberties(board, x-1, y))
+				removegroup(board, x-1, y);
+			if(x<18 && !hasliberties(board, x+1, y))
+				removegroup(board, x+1, y);
+			if(y>1 && !hasliberties(board, x, y-1))
+				removegroup(board, x, y-1);
+			if(y<18 && !hasliberties(board, x, y+1))
+				removegroup(board, x, y+1);
+			if(memcmp(temp, board, sizeof(temp)))
+				DrawBoard(dc);
+		}
 	}
+	printboard(board);
 }
 
 bool MyApp::OnInit()
 {
-	frame = new MainFrame(wxT("Go Points"), wxPoint(-1, -1), wxSize(700, 700));
+	frame = new MainFrame(wxT("Go Points"), wxPoint(-1, -1), wxSize(350, 350));
 	/*x = 0;
 	y = 18;
 	x = rand()%19;
 	y = rand()%19;*/
 	turn = 1;
+	for(int i=0; i<19; i++)
+		for(int j=0; j<19; j++)
+			board[i][j] = 0;
 	
 	frame->Show();
 	SetTopWindow(frame);
