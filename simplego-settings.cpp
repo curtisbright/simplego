7020c9e843be6b49e46811b906edb4312cedc4b4
Curtis Bright <cbright@uwaterloo.ca>
2014-01-03 02:41:01 -0500

    more controls in settings dialog

--- simplego-settings.cpp
+++ simplego-settings.cpp
@@ -2,15 +2,32 @@
 #include "simplego-settings.h"
 
 // Settings dialog constructor
-SimpleGoSettingsDialog::SimpleGoSettingsDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, wxT("Settings"), wxPoint(80,80), wxSize(200,200))
+SimpleGoSettingsDialog::SimpleGoSettingsDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, wxT("Settings"))
 {	wxBoxSizer* vsizer = new wxBoxSizer(wxVERTICAL);
 	SetSizer(vsizer);
-	wxBoxSizer* blackhsizer = new wxBoxSizer(wxHORIZONTAL);
-	wxBoxSizer* whitehsizer = new wxBoxSizer(wxHORIZONTAL);
-	vsizer->Add(blackhsizer);
-	vsizer->Add(whitehsizer);
-	wxStaticText* blackprompt = new wxStaticText(this, wxID_ANY, "Black Name:");
-	wxStaticText* whiteprompt = new wxStaticText(this, wxID_ANY, "White Name:");
-	blackhsizer->Add(blackprompt);
-	whitehsizer->Add(whiteprompt);
+	wxBoxSizer* blackhsizer = new wxStaticBoxSizer(wxHORIZONTAL, this, "Black");
+	wxBoxSizer* whitehsizer = new wxStaticBoxSizer(wxHORIZONTAL, this, "White");
+	vsizer->Add(blackhsizer, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	vsizer->Add(whitehsizer, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	wxStaticText* blackprompt = new wxStaticText(this, wxID_ANY, "Name:");
+	wxStaticText* whiteprompt = new wxStaticText(this, wxID_ANY, "Name:");
+	blackhsizer->Add(blackprompt, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	whitehsizer->Add(whiteprompt, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	wxTextCtrl* blackname = new wxTextCtrl(this, wxID_ANY);
+	wxTextCtrl* whitename = new wxTextCtrl(this, wxID_ANY);
+	blackhsizer->Add(blackname, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	whitehsizer->Add(whitename, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	wxCheckBox* blackgnugo = new wxCheckBox(this, wxID_ANY, "GNU Go");
+	wxCheckBox* whitegnugo = new wxCheckBox(this, wxID_ANY, "GNU Go");
+	blackhsizer->Add(blackgnugo, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	whitehsizer->Add(whitegnugo, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	wxString levels[11] = {"Default level", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};        
+	wxArrayString* str = new wxArrayString(11, levels);
+	wxChoice* blacklevel = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, *str);
+	wxChoice* whitelevel = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, *str);
+	blacklevel->SetSelection(0);
+	whitelevel->SetSelection(0);
+	blackhsizer->Add(blacklevel, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	whitehsizer->Add(whitelevel, 0, wxALL | wxALIGN_CENTRE_VERTICAL, 5);
+	vsizer->Fit(this);
 }
